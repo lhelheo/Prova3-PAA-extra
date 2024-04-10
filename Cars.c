a8e38fa11027de75d442cf1938ddba80cab756a3
Leonardo Oliveira Villalva <leonardo.villalva@academico.ufs.br>
2024-04-10 00:58:45 -0300

    correção da função de intervalos entre os carros para saber a quantidade mecânicos

--- Cars.c
+++ Cars.c
@@ -1,61 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Estrutura para representar um carro com o tempo de início e fim de disponibilidade
 typedef struct {
-    int s; // Tempo de início de disponibilidade
-    int f; // Tempo de fim de disponibilidade
-} Carro;
+    int inicio; 
+    int fim;    
+} Disponibilidade;
 
-// Função para comparar dois carros com base no tempo de término de disponibilidade
-int CompararCarros(const void *a, const void *b) {
-    Carro *carro1 = (Carro*)a;
-    Carro *carro2 = (Carro*)b;
-    return carro1->f - carro2->f;
+int compararDisponibilidade(Disponibilidade dispA, Disponibilidade dispB) {
+    if (dispA.fim != dispB.fim)
+        return dispA.fim - dispB.fim;
+    return dispA.inicio - dispB.inicio;
 }
 
-// Função para ordenar os carros com base no tempo de término de disponibilidade
-void OrdenarCarros(Carro carros[], int n) {
-    qsort(carros, n, sizeof(Carro), CompararCarros);
+void ordenarPorInsercao(Disponibilidade disponibilidades[], int n) {
+    int i, j;
+    Disponibilidade chave;
+    for (i = 1; i < n; i++) {
+        chave = disponibilidades[i];
+        j = i - 1;
+
+        while (j >= 0 && compararDisponibilidade(disponibilidades[j], chave) > 0) {
+            disponibilidades[j + 1] = disponibilidades[j];
+            j = j - 1;
+        }
+        disponibilidades[j + 1] = chave;
+    }
 }
 
-// Função para alocar mecânicos para os carros
-int AlocaMecanicos(int s[], int f[], int n) {
-    // Criar um array de carros com os tempos de início e fim de disponibilidade
-    Carro carros[n];
+int alocarMecanicos(Disponibilidade disponibilidades[], int n) {
+    ordenarPorInsercao(disponibilidades, n);
+
+    int mecanicosNecessarios = n; 
+    int considerado[n]; 
+
     for (int i = 0; i < n; i++) {
-        carros[i].s = s[i];
-        carros[i].f = f[i];
+        considerado[i] = 0; 
     }
 
-    // Ordenar os carros com base no tempo de término de disponibilidade
-    OrdenarCarros(carros, n);
-
-    // Alocar mecânicos para os carros
-    int mecanicos_utilizados = 1; // Inicialmente, pelo menos um mecânico é necessário
-    int tempo_atual = carros[0].f; // Tempo atual é o fim da disponibilidade do primeiro carro
-
-    for (int i = 1; i < n; i++) {
-        if (carros[i].s >= tempo_atual) { // Verifica se o próximo carro não entra em conflito com o atual
-            mecanicos_utilizados++; // Aumenta o número de mecânicos utilizados
-            tempo_atual = carros[i].f; // Atualiza o tempo atual para o fim da disponibilidade do próximo carro
+    for (int i = 0; i < n; i++) {
+        if (!considerado[i]) { 
+            for (int j = i + 1; j < n; j++) {
+                if (disponibilidades[j].inicio >= disponibilidades[i].inicio && disponibilidades[j].fim <= disponibilidades[i].fim) {
+                    mecanicosNecessarios--; 
+                    considerado[j] = 1; 
+                }
+                else if (disponibilidades[j].inicio > disponibilidades[i].fim && disponibilidades[j].fim > disponibilidades[i].fim) {
+                    break;
+                }
+            }
         }
     }
 
-    return mecanicos_utilizados;
+    return mecanicosNecessarios;
 }
 
 int main() {
-    // Exemplo de disponibilidade de carros
-    int s[] = {1, 3, 4, 6};
-    int f[] = {5, 5, 7, 8};
-    int n = sizeof(s) / sizeof(s[0]);
+    int s[] = {1, 2, 3, 6, 7}; 
+    int f[] = {7, 8, 9, 8, 9}; 
+    int n = sizeof(s) / sizeof(s[0]); 
 
-    // Chama a função para alocar mecânicos para os carros
-    int mecanicos = AlocaMecanicos(s, f, n);
+    Disponibilidade disponibilidades[n];
+    for (int i = 0; i < n; i++) {
+        disponibilidades[i].inicio = s[i];
+        disponibilidades[i].fim = f[i];
+    }
 
-    // Imprime o número de mecânicos utilizados
-    printf("Numero de mecanicos utilizados: %d\n", mecanicos);
+    int mecanicos = alocarMecanicos(disponibilidades, n);
+    printf("Numero de mecanicos necessarios: %d\n", mecanicos);
 
     return 0;
 }
